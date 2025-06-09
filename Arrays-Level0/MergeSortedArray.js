d428cfb3dafeb3253b5b5fa5913e1c7d604d6346
vineela-ws <amaranenivineela54@gmail.com>
2025-06-09 11:04:37 +0530

    MergeSortedArray

---
+++ Arrays-Level0/MergeSortedArray.js
@@ -0,0 +1,28 @@
+
+function mergeSortedArray(nums1,m,nums2,n){
+
+    let p1 = m-1;
+    let p2 = n-1;
+
+    for(let i = m+n-1; i>=0; i--){
+        if(p2<0) break;
+
+        if(p1>=0 && nums1[p1]>nums2[p2]){
+            nums1[i] = nums1[p1];
+            p1--;
+        }else{
+            nums1[i] = nums2[p2];
+            p2--;
+        }
+    }
+    return nums1;
+}
+
+let nums1 = [1,2,3,0,0,0];
+let nums2  = [2,5,6];
+let m = 3;
+let n = 3;
+
+let result = mergeSortedArray(nums1,m,nums2,n);
+console.log(result);
+
